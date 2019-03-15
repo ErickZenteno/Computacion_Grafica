fc126e0cf192b28373e7b03c565e16e5f6161b40
CGI06 <CGI06>
2019-03-15 11:45:33 -0600

    correccionShader

--- Shaders/transformaciones.fs
+++ Shaders/transformaciones.fs
@@ -1,18 +1,13 @@
 #version 330 core
 
 in vec3 our_color;
-<<<<<<< HEAD
 
-=======
->>>>>>> e24aebae62b3431ea597760cd924bfc50197c85c
 out vec4 color;
 
 void main(){
 	
-<<<<<<< HEAD
+
 	color=vec4(our_color,1.0);
-=======
-	color = vec4(our_color, 1.0);
->>>>>>> e24aebae62b3431ea597760cd924bfc50197c85c
+
 
 }
